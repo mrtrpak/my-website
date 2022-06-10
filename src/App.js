eb8cb70abe548e4f58bf253d81db0eb2e344bb2a
Michael Trpak <mrtrpak3@gmail.com>
2022-06-10 16:44:57 -0400

    able to make the api call and get the data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import axios from 'axios';
+import React from 'react';
+
 export default function App() {
+  let [standings, setStandings] = React.useState(null);
+
+  React.useEffect(() => {
+    axios({
+      "url": "https://api.football-data.org/v2/competitions/BL1/standings",
+      "headers": {
+        "X-Auth-Token": "5d08ba09e1d84003adc242dbd50ca5bb"
+      }
+    })
+    .then(res => {
+      setStandings(res.data);
+    })
+    .catch(err => console.log(err));
+  }, []);
+  console.log(standings, "standings");
+
   return (
     <div className="App">
-      my website
+      <h1>Fetching Data with React Hooks</h1>
     </div>
   );
-}
+};
