5f6ea2e299bbfad372847f7d81824c57e5bf6639
Michael Trpak <mrtrpak3@gmail.com>
2022-06-15 17:06:43 -0400

    adding env variable

--- src/components/soccer/soccer-standings.js
+++ src/components/soccer/soccer-standings.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { ButtonGroup, Button } from '@mui/material';
 
-import { soccerKey } from '../../secret';
+const soccerKey = process.env.soccerKey;
 
 import SoccerHeader from './soccer-header';
 
@@ -10,24 +10,20 @@ export default function SoccerStandings() {
   let [league, setLeague] = React.useState("BL1");
   let [standings, setStandings] = React.useState(null);
 
-  const setLeagueFunc = (league) => {
-
-  }
-
-  console.log(league)
+  console.log(league);
 
   React.useEffect(() => {
     setLeagueFunc();
-    // axios({
-    //   "url": `https://api.football-data.org/v2/competitions/${league}/standings`,
-    //   "headers": {
-    //     "X-Auth-Token": soccerKey
-    //   }
-    // })
-    // .then(res => {
-    //   setStandings(res.data.standings[0].table);
-    // })
-    // .catch(err => console.log(err));
+    axios({
+      "url": `https://api.football-data.org/v2/competitions/${league}/standings`,
+      "headers": {
+        "X-Auth-Token": soccerKey
+      }
+    })
+    .then(res => {
+      setStandings(res.data.standings[0].table);
+    })
+    .catch(err => console.log(err));
   }, []);
 
   console.log(standings, "standings");
